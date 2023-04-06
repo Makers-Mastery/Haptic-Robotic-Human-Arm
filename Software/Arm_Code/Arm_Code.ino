3a330ff8b95f90f486a0c7b6c8850f166278488f
Elijah Hansen <60437999+Makers-Mastery@users.noreply.github.com>
2023-04-06 10:21:29 -0600

    Update Arm_Code.ino

--- Software/Arm_Code/Arm_Code.ino
+++ Software/Arm_Code/Arm_Code.ino
@@ -1,16 +1,17 @@
 /*
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
-
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
-
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 */
 
 #include <esp_now.h>
 #include <WiFi.h>
+
+#include <ESP32Servo.h>
+#include <Arduino.h>
 
 // Must specify this before the include of "ServoEasing.hpp"
 #define USE_PCA9685_SERVO_EXPANDER // Activate this to enables the use of the PCA9685 I2C expander chip/board.
@@ -24,13 +25,27 @@
 //#define SERVOMIN  100 // This is the 'minimum' pulse length count (out of 4096)
 //#define SERVOMAX  500 // This is the 'maximum' pulse length count (out of 4096)
 
+//LETS DECLARE PCA BOARDS FOR LEFT ARM AND RIGHT ARM
+char wristElbowArm = 0x40;
+char shoulderAssem = 0x41;
+
 //servo pins
 //check pins
 #define shoulderOutServo 0
 #define shoulderForServo 1
 #define armRotServo 2
-#define elbowServo 3
-#define wristServo 4
+#define elbowServo 1
+#define wristServo 0
+
+#define servo_speed 180
+
+
+//LETS DECLARE ALL SERVO INDEXES WHICH WE WILL USE IN THE StartEaseTo METHO
+int armAssemWrist;
+int armAssemElbow;
+int armAssemArm;
+int shouldAssemOut;
+int shouldAssemFor;
 
 #define servo_speed 60
 
@@ -87,33 +102,48 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.print("Bool: ");
   Serial.println(potValues.d);
   Serial.println();
-
-  moveArm();
 }
 
 void setup() {
   // Initialize Serial Monitor
-  Serial.begin(115200);
+  Serial.begin(9600);
   Wire.begin();
   ServoEasing *ServoMaster;
 
   if (checkI2CConnection(PCA9685_DEFAULT_ADDRESS, &Serial)) {
     Serial.println(F("PCA9685 expander not connected"));
   } else {
-    ServoMaster = new ServoEasing(PCA9685_DEFAULT_ADDRESS, &Wire);
-    ServoMaster->attach(shoulderOutServo);
-    ServoMaster = new ServoEasing(PCA9685_DEFAULT_ADDRESS, &Wire)
-    ServoMaster->attach(shoulderForServo);
-    ServoMaster = new ServoEasing(PCA9685_DEFAULT_ADDRESS, &Wire);
-    ServoMaster->attach(armRotServo);
-    ServoMaster = new ServoEasing(PCA9685_DEFAULT_ADDRESS, &Wire);
-    ServoMaster->attach(elbowServo);
-    ServoMaster = new ServoEasing(PCA9685_DEFAULT_ADDRESS, &Wire);
+    ServoMaster = new ServoEasing(wristElbowArm, &Wire);
     ServoMaster->attach(wristServo);
-    /*
-      if ((indexServo || middleServo || ringServo || pinkyServo || thumbServo || thumbRotServo) == INVALID_SERVO) {
-      Serial.println(F("Error attaching servo index_finger - maybe MAX_EASING_SERVOS=" STR(MAX_EASING_SERVOS) " is to small to hold all servos"));
-      } */
+    armAssemWrist = 0; //the value is 0 because its the first attached
+
+    ServoMaster = new ServoEasing(wristElbowArm, &Wire);
+    ServoMaster->attach(elbowServo);
+    armAssemElbow = 1; //the value is 1 because its the second attached
+
+    ServoMaster = new ServoEasing(wristElbowArm, &Wire);
+    ServoMaster->attach(armRotServo);
+    armAssemArm = 2; //the value is 2 because its the third attached
+
+    ServoMaster = new ServoEasing(shoulderAssem, &Wire);
+    ServoMaster->attach(shoulderForServo);
+    shouldAssemFor = 3; //the value is 3 because its the fourth attached
+
+    ServoMaster = new ServoEasing(shoulderAssem, &Wire);
+    ServoMaster->attach(shoulderOutServo);
+    shouldAssemOut = 4; //the value is 4 because its the fifth attached
+
+    ServoEasing::ServoEasingArray[armAssemWrist]->setEasingType(EASE_QUADRATIC_IN);
+    ServoEasing::ServoEasingArray[armAssemElbow]->setEasingType(EASE_QUADRATIC_IN);
+    ServoEasing::ServoEasingArray[armAssemArm]->setEasingType(EASE_QUADRATIC_IN);
+    ServoEasing::ServoEasingArray[shouldAssemFor]->setEasingType(EASE_QUADRATIC_IN);
+    ServoEasing::ServoEasingArray[shouldAssemOut]->setEasingType(EASE_QUADRATIC_IN);
+
+    ServoEasing::ServoEasingArray[armAssemWrist]->setSpeed(servo_speed);
+    ServoEasing::ServoEasingArray[armAssemElbow]->setSpeed(servo_speed);
+    ServoEasing::ServoEasingArray[armAssemArm]->setSpeed(servo_speed);
+    ServoEasing::ServoEasingArray[shouldAssemFor]->setSpeed(servo_speed);
+    ServoEasing::ServoEasingArray[shouldAssemOut]->setSpeed(servo_speed);
   }
 
 
@@ -132,19 +162,36 @@ void setup() {
 }
 
 void loop() {
-
+  moveArm();
 }
 
 void moveArm() {
-  shoulderOutRotVal = map(potValues.shoulderOutPotV, 50, 650, 30, 170);
-  shoulderForRotVal = map(potValues.shoulderForPotV, 50, 650, 30, 170);
-  armRotServVal = map(potValues.armRotPotV, 50, 650, 30, 170);
-  elbowRotVal = map(potValues.elbowPotV, 50, 650, 30, 170);
-  wristRotVal = map(potValues.wristPotV, 50, 650, 30, 170);
+  //servo pos
+  //wrist 28 to 206
+  //elbow 100 to 200
+  // arm rot 85 to 185
+  //shoulder out 128 to 200 (keep in mind the linkage makes translation weird)
+  //shoulder forward 85 to 177
 
-  ServoEasing::ServoEasingArray[shoulderOutServo]->startEaseTo(shoulderOutRotVal); //async method (means that you can rotate more then one servo the same time (the method bellow is rotating one servo at a time))
-  ServoEasing::ServoEasingArray[shoulderForServo]->startEaseTo(shoulderForRotVal);
-  ServoEasing::ServoEasingArray[armRotServo]->startEaseTo(armRotServVal);
-  ServoEasing::ServoEasingArray[elbowServo]->startEaseTo(elbowRotVal);
-  ServoEasing::ServoEasingArray[wristServo]->startEaseTo(wristRotVal);
+  //arm pots
+  //wrist 1300 left to 3700 right
+  //elbow 300 down to 1300 bent
+  //armRot 700 in to 1500 out
+  //shoulderFor 2000 back 2500 forward
+  //shoulderOut 1500 down to 3800 out
+
+  //                        map, pot, servo
+  //  indexRotVal = map(incomingPotReadings.index, 1100, 4095, 55, 180);
+  shoulderOutRotVal = map(potValues.shoulderOutPotV, 3800, 1500, 128, 200);
+  shoulderForRotVal = map(potValues.shoulderForPotV, 2500, 2000, 85, 177);
+  armRotServVal = map(potValues.armRotPotV, 1500, 700, 85, 185);
+  elbowRotVal = map(potValues.elbowPotV, 1300, 300, 100, 200);
+  wristRotVal = map(potValues.wristPotV, 1300, 3700, 28, 206);
+
+  //NOW HERE WHERE YOU WANT TO ROTATE THE SERVOS YOU USE THE DECLARED INDEXES FROM THE SETUP
+  ServoEasing::ServoEasingArray[shouldAssemOut]->startEaseTo(shoulderOutRotVal); //async method (means that you can rotate more then one servo the same time (the method bellow is rotating one servo at a time))
+  ServoEasing::ServoEasingArray[shouldAssemFor]->startEaseTo(shoulderForRotVal);
+  ServoEasing::ServoEasingArray[armAssemArm]->startEaseTo(armRotServVal);
+  ServoEasing::ServoEasingArray[armAssemElbow]->startEaseTo(elbowRotVal);
+  ServoEasing::ServoEasingArray[armAssemWrist]->startEaseTo(wristRotVal);
 }
